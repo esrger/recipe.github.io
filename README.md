f675e45c32251859ea4dfa6f1912d190203cb353
praetorian <praetorian@Asus>
2017-03-26 05:23:10 +0300

    Test gitpages example

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# tictactoe.github.io
-https://github.com/praetorianlive/tictactoe.github.io
-https://praetorianlive.github.io/tictactoe.github.io/
+# Deployment
+Angular 2 Deployment in Github Pages
+
+Testing Deployment of Angular2 Webpack in ghpages
+
+First get all relevant the files from the Dist Folder of your application 
+
+for me it was the css files in the assets folder 
+main.bundle.js
+polyfills.bundle.js
+vendor.bundle.js
+
+Then push this files in the repo which you have created.
+
+1 -- If you want the application to run on the root directory - create a special repo with the name [yourgithubusername].github.io and opush these files
+in the master branch 
+
+2 -- Where as if you want to create these page in the sub directory of in a different brach other than than the root , create a branch gh-pages and push these files in that branch.
+
+In both the cases the way we access these deployed pages will be different .
+
+For the First Case it will be https://[yourgithubusername].github.io and 
+for the second case it will be [yourgithubusername].github.io/[Repo name].
+
+
+If Suppose you want to deploy it using the sefcond case make sure to change the base url  pf the index.html file in the dist as all the route mappings depend on the path you give and it should be set to [/branchname].
+
+
+Link to this page
+
+https://rahulrsingh09.github.io/Deployment
+
+
+
---
+++ main.bundle.js
@@ -0,0 +1,173 @@
+var ac_main =
+webpackJsonpac__name_([1],{
+
+/***/ 373:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+// Angular 2
+// rc2 workaround
+var platform_browser_1 = __webpack_require__(70);
+var core_1 = __webpack_require__(0);
+// Environment Providers
+var PROVIDERS = [];
+// Angular debug tools in the dev console
+// https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
+var _decorateModuleRef = function identity(value) { return value; };
+if (false) {
+    core_1.enableProdMode();
+    // Production
+    _decorateModuleRef = function (modRef) {
+        platform_browser_1.disableDebugTools();
+        return modRef;
+    };
+    PROVIDERS = PROVIDERS.slice();
+}
+else {
+    _decorateModuleRef = function (modRef) {
+        var appRef = modRef.injector.get(core_1.ApplicationRef);
+        var cmpRef = appRef.components[0];
+        var _ng = window.ng;
+        platform_browser_1.enableDebugTools(cmpRef);
+        window.ng.probe = _ng.probe;
+        window.ng.coreTokens = _ng.coreTokens;
+        return modRef;
+    };
+    // Development
+    PROVIDERS = PROVIDERS.slice();
+}
+exports.decorateModuleRef = _decorateModuleRef;
+exports.ENV_PROVIDERS = PROVIDERS.slice();
+
+
+/***/ },
+
+/***/ 374:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+// App
+__export(__webpack_require__(497));
+
+
+/***/ },
+
+/***/ 496:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__(0);
+var AppComponent = (function () {
+    function AppComponent() {
+        this.name = 'Angular';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'app',
+        template: "<h1>Hello {{name}}</h1>",
+    }),
+    __metadata("design:paramtypes", [])
+], AppComponent);
+exports.AppComponent = AppComponent;
+
+
+/***/ },
+
+/***/ 497:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__(0);
+var platform_browser_1 = __webpack_require__(70);
+var app_component_1 = __webpack_require__(496);
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [platform_browser_1.BrowserModule],
+        declarations: [app_component_1.AppComponent],
+        bootstrap: [app_component_1.AppComponent]
+    }),
+    __metadata("design:paramtypes", [])
+], AppModule);
+exports.AppModule = AppModule;
+
+
+/***/ },
+
+/***/ 659:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+/*
+ * Angular bootstraping
+ */
+var platform_browser_dynamic_1 = __webpack_require__(146);
+var environment_1 = __webpack_require__(373);
+var hmr_1 = __webpack_require__(147);
+/*
+ * App Module
+ * our top level module that holds all of our components
+ */
+var app_1 = __webpack_require__(374);
+/*
+ * Bootstrap our Angular app with a top level NgModule
+ */
+function main() {
+    return platform_browser_dynamic_1.platformBrowserDynamic()
+        .bootstrapModule(app_1.AppModule).then(function(MODULE_REF) {
+  if (false) {
+    module["hot"]["accept"]();
+    
+    if (MODULE_REF.instance["hmrOnInit"]) {
+      module["hot"]["data"] && MODULE_REF.instance["hmrOnInit"](module["hot"]["data"]);
+    }
+    if (MODULE_REF.instance["hmrOnStatus"]) {
+      module["hot"]["apply"](function(status) {
+        MODULE_REF.instance["hmrOnStatus"](status);
+      });
+    }
+    if (MODULE_REF.instance["hmrOnCheck"]) {
+      module["hot"]["check"](function(err, outdatedModules) {
+        MODULE_REF.instance["hmrOnCheck"](err, outdatedModules);
+      });
+    }
+    if (MODULE_REF.instance["hmrOnDecline"]) {
+      module["hot"]["decline"](function(dependencies) {
+        MODULE_REF.instance["hmrOnDecline"](dependencies);
+      });
+    }
+    module["hot"]["dispose"](function(store) {
+      MODULE_REF.instance["hmrOnDestroy"] && MODULE_REF.instance["hmrOnDestroy"](store);
+      MODULE_REF.destroy();
+      MODULE_REF.instance["hmrAfterDestroy"] && MODULE_REF.instance["hmrAfterDestroy"](store);
+    });
+  }
+  return MODULE_REF;
+})
+        .then(environment_1.decorateModuleRef)
+        .catch(function (err) { return console.error(err); });
+}
+exports.main = main;
+// needed for hmr
+// in prod this is replace for document ready
+hmr_1.bootloader(main);
+
+
+/***/ }
+
+},[659]);
+//# sourceMappingURL=main.map
